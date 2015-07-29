33678f7dd459f5027af52e035e92806249cf98cc
gerar_000 <gerard.aguilar.156@my.csun.edu>
2015-07-29 16:55:29 -0700

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.jarrodgeraldsgarage.simpletodolist;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                Log.d("MainActivity", "Add a new task");
+                return true;
+
+            default:
+                return false;
+        }
+    }
+}

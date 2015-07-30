4ac8cb05e3940b073ca20fb991d61d8b315129e7
gerar_000 <gerard.aguilar.156@my.csun.edu>
2015-07-29 17:35:48 -0700

    1. Simple To Do List to learn sqlLite

--- MainActivity.java
+++ MainActivity.java
@@ -1,18 +1,50 @@
 package com.example.jarrodgeraldsgarage.simpletodolist;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import com.example.jarrodgeraldsgarage.simpletodolist.db.TaskContract;
+import com.example.jarrodgeraldsgarage.simpletodolist.db.TaskDBHelper;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private TaskDBHelper helper;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        SQLiteDatabase sqlDB = new TaskDBHelper(this).getWritableDatabase();
+//        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+//                new String[]{TaskContract.Columns.TASK},
+//                null,null,null,null,null);
+//
+//        cursor.moveToFirst();
+//        while(cursor.moveToNext()) {
+//            Log.d("MainActivity cursor",
+//                    cursor.getString(
+//                            cursor.getColumnIndexOrThrow(
+//                                    TaskContract.Columns.TASK)));
+//        }
+        updateUI();
+
     }
 
     @Override
@@ -25,11 +57,78 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                Log.d("MainActivity", "Add a new task");
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add a task");
+                builder.setMessage("What do you want to do?");
+                final EditText inputField = new EditText(this);
+                builder.setView(inputField);
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Log.d("MainActivity",inputField.getText().toString());
+                        String task = inputField.getText().toString();
+                        Log.d("MainActivity",task);
+
+                        TaskDBHelper helper = new TaskDBHelper(MainActivity.this);
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+
+                        values.clear();
+                        values.put(TaskContract.Columns.TASK, task);
+
+                        db.insertWithOnConflict(TaskContract.TABLE, null, values,
+                                SQLiteDatabase.CONFLICT_IGNORE);
+
+                        updateUI();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel",null);
+
+                builder.create().show();
                 return true;
+
+            // ... other code
 
             default:
                 return false;
         }
+    }
+
+    private void updateUI() {
+        helper = new TaskDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+                new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK},
+                null, null, null, null, null);
+
+        ListAdapter listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.task_view,
+                cursor,
+                new String[] { TaskContract.Columns.TASK},
+                new int[] { R.id.taskTextView},
+                0
+        );
+        ListView listView = (ListView)findViewById(R.id.list);
+        listView.setAdapter(listAdapter);
+//        this.setListAdapter(listAdapter);
+    }
+
+    public void onDoneButtonClick(View view) {
+        View v = (View) view.getParent();
+        TextView taskTextView = (TextView) v.findViewById(R.id.taskTextView);
+        String task = taskTextView.getText().toString();
+
+        String sql = String.format("DELETE FROM %s WHERE %s = '%s'",
+                TaskContract.TABLE,
+                TaskContract.Columns.TASK,
+                task);
+
+
+        helper = new TaskDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = helper.getWritableDatabase();
+        sqlDB.execSQL(sql);
+        updateUI();
     }
 }
---
+++ db/TaskContract.java
@@ -0,0 +1,17 @@
+package com.example.jarrodgeraldsgarage.simpletodolist.db;
+
+/**
+ * Created by gerar_000 on 7/29/2015.
+ */
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.jarrodgeraldsgarage.simpletodolist.db";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;
+    }
+}
